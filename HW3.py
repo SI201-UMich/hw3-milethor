da5f69c7ac410488b099f4406977e99681c876b8
unknown <milethor@umich.edu>
2026-01-28 16:27:01 -0500

    __init__ function

--- HW3.py
+++ HW3.py
@@ -1,6 +1,6 @@
-# Name:
-# Student ID:
-# Email:
+# Name: Miles Thornton
+# Student ID: 4762 4083
+# Email:milethor@umich.edu
 # Who or what you worked with on this homework (including generative AI like ChatGPT):
 # If you worked with generative AI also add a statement for how you used it.
 # e.g.:
@@ -33,7 +33,9 @@ class CouponDispenser:
             coupon_cards (list[str]): list of possible coupons users can receive.
         """
         # TODO: Implement per instructions
-        pass
+        self.coupon_cards = coupon_cards
+        self.customer_roster = []
+        self.issued_indices = []
 
     def __str__(self):
         """
